dd016bc3318edaa8a46a4007ed84d4bf746afe40
vique254 <victoronyango051@gmail.com>
2019-11-01 15:26:01 +0300

    create a form component

---
+++ src/app/github-search-form/github-search-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-search-form',
+  templateUrl: './github-search-form.component.html',
+  styleUrls: ['./github-search-form.component.css']
+})
+export class GithubSearchFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
