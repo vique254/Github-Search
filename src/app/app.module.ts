8ad95858d9ba6bdb82399f75c4f45ae2a83d2bb5
vique254 <victoronyango051@gmail.com>
2019-11-03 23:56:38 +0300

    create import/export

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GithubSearchFormComponent } from './github-search-form/github-search-form.component';
+import { GithubFormComponent } from './github-form/github-form.component';
 import { AboutComponent } from './about/about.component';
 import { GithubComponent } from './github/github.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -11,14 +13,16 @@ import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    GithubSearchFormComponent,
+    GithubFormComponent,
     AboutComponent,
     GithubComponent,
     NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
