397f273e1d493ba65ecceeb8a6f41093d0033246
vique254 <victoronyango051@gmail.com>
2019-11-03 19:13:13 +0300

    add more imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubSearchFormComponent } from './github-search-form/github-search-form.component';
+import { AboutComponent } from './about/about.component';
+import { GithubComponent } from './github/github.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubSearchFormComponent
+    GithubSearchFormComponent,
+    AboutComponent,
+    GithubComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
