134e3fa2e4afeca9daeec46137d424c59cd5fc3e
vique254 <victoronyango051@gmail.com>
2019-11-04 10:21:36 +0300

    add content for service

--- src/app/github-service/github.service.ts
+++ src/app/github-service/github.service.ts
@@ -1,74 +1,38 @@
+ 
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
-import { Github } from '../github';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
-import { Observable } from 'rxjs/internal/Observable';
-import { observable } from 'rxjs';
+import {HttpClient} from'@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Github} from '../github';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class GithubService {
-    private userName: string;
-    private clientId: string;
-    private clientSecret: string;
-    private getUserUrl: string;
-    private getRepoUrl: string;
-    private getReadmeUrl: string;
+  githubs:Github[]=[];
+  constructor(private http:HttpClient) { }
 
-    constructor(private _http: Http) {
-        this.userName = '';
-        this.clientId = '60b9f23dedffbdfc476c';
-        this.clientSecret = 'd1c186c6373f96571c0bfcf76b84e4dc6fd0c15a';
-        this.getUserUrl = 'https://api.github.com/users/[userName]?client_id=[clientId]&client_secret=[clientSecret]';
-        this.getRepoUrl = 'https://api.github.com/users/[userName]/repos?client_id=[clientId]&client_secret=[clientSecret]';
-        this.getReadmeUrl = 'https://api.github.com/repos/[userName]/[repo]/readme?client_id=[clientId]&client_secret=[clientSecret]';
-    }
-
-    getUser(userName) {
-        this.userName = userName;
-        if (this.userName) {
-            return this._http.get(this.getUserUrl.replace('[userName]', this.userName)
-                .replace('[clientId]', this.clientId)
-                .replace('[clientSecret]', this.clientSecret))
-                .map(res => res.json())
-                .catch(this.handleError);
-        }
-    }
-
-    getRepos(userName) {
-        this.userName = userName;
-        if (this.userName) {
-            return this._http.get(this.getRepoUrl.replace('[userName]', this.userName)
-                .replace('[clientId]', this.clientId)
-                .replace('[clientSecret]', this.clientSecret))
-                .map(res => res.json())
-                .catch(this.handleError);
-        }
-    }
-
-    getReadme(repoName: string, userName: string) {
-        this.userName = userName;
-        if (this.userName) {
-            return this._http.get(this.getReadmeUrl.replace('[userName]', this.userName)
-                .replace('[clientId]', this.clientId)
-                .replace('[clientSecret]', this.clientSecret)
-                .replace('[repo]', repoName))
-                .map(res => res.json())
-                .catch(this.handleError);
-        }
-    }
-
-    updateUsername(userName: string) {
-        this.userName = userName;
-    }
-
-    private handleError(error: any) {
-
-        if (error.status === 401) {
-            return Observable.throw(error.status);
-        } else {
-            return observable.throw(error.status || 'Server error');
-        }
-    }
+  searchGithubs(searchTerm:string){
+    
+    let searchEndpoint= "https://api.github.com/v1/git/search?api_key="+environment.GITHUBAPIKEY;
+    searchEndpoint += "&q="+searchTerm;
+    let promise =  new Promise((resolve, reject)=>{
+        this.http.get(searchEndpoint).toPromise().then(
+          (results)=>{
+            this.githubs=[];
+            for(let i=0; i<results["data"].length; i++){
+              let url = results["data"][i]["images"]["fixed_height"]["url"];
+              let github = new Github(url);
+              this.githubs.push(github);
+            }
+            console.log(this.githubs);
+            resolve()
+          },
+          (error)=>{
+            console.log(error)
+            reject()
+          }
+        )
+    })
+    return promise;
+  }
 }
